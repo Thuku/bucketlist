3486bec991a4d7fdb02702b141941e8b0372ea4f
thuku <thuku.bonnie@gmail.com>
2017-06-22 16:37:25 +0300

    Remove cached file

---
+++ tests/test_app_routes.py
@@ -0,0 +1,12 @@
+import unittest
+
+class FlaskAppTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    
+
+
+
+    def tearDown(self):
+        pass
--- tests/test_user_module.py
+++ tests/test_user_module.py
@@ -1,12 +1,29 @@
+"""
+Tests for the users module
+"""
 import unittest
 
 
-class AccountsTests(unittest.TestCase):
+class UserModuleTests(unittest.TestCase):
+    """
+    Class tests
+    """
     def test_empty_input(self):
+        """
+        Test for empty user inputs
+        """
         pass
 
     def test_password_confirmation(self):
+        """
+        test whether password and confirm password match
+        """
         pass
 
     def test_user_exists(self):
+        """
+        Test whether the user exists
+        """
+        pass
+    def test_user_email_is_correct(self):
         pass
