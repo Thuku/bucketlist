4203af116784e2725b816c08460097806f43cee2
thuku <thuku.bonnie@gmail.com>
2017-06-22 14:31:55 +0300

    Implement create bucketlist and bucketlist activities

---
+++ app/bucketlist_acc.py
@@ -0,0 +1,11 @@
+class BucketLists(object):
+    bucketlists = []
+    def __init__(self):
+        pass
+
+
+    def create_bucket_list(self, bucketlist_name, activity):
+        bucketlist = {"name": bucketlist_name, "activity": activity}
+        self.bucketlists.append(bucketlist)
+        print(self.bucketlists)
+        return bucketlist["name"]
